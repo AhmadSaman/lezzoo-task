9b5974cffb6fd3f1f3af967221b2167715c3e573
Ahmad Saman Abdulhadi <ahmad.00079821@gmail.com>
2022-01-07 13:06:30 +0300

    setup react-router-dom

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,15 @@
 import { Button, Text } from "@chakra-ui/react";
+import { Switch, Route } from "react-router-dom";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <div className="App">
       <Text>hey</Text>
       <Button variant={"solid"}>click</Button>
+      <Switch>
+        <Route path={"/home"} component={() => <Home />} />
+      </Switch>
     </div>
   );
 }
